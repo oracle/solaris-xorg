14ed0ce48c5b7dd4ccb93686747c0211ac947904
Alan Coopersmith <Alan.Coopersmith@Sun.COM>
2008-02-27 19:55:52 -0800

    6667480 force_rebuild property for fc-cache SMF service [PSARC 2008/106]

---
+++ open-src/lib/fontconfig/fc-cache.sh
@@ -0,0 +1,96 @@
+#!/bin/ksh
+#
+# Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, and/or sell copies of the Software, and to permit persons
+# to whom the Software is furnished to do so, provided that the above
+# copyright notice(s) and this permission notice appear in all copies of
+# the Software and that both the above copyright notice(s) and this
+# permission notice appear in supporting documentation.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT
+# OF THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# HOLDERS INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL
+# INDIRECT OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING
+# FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+# WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+# Except as contained in this notice, the name of a copyright holder
+# shall not be used in advertising or otherwise to promote the sale, use
+# or other dealings in this Software without prior written authorization
+# of the copyright holder.
+#
+###########################################################################
+#
+# ident	"@(#)fc-cache.sh	1.1 08/02/24 SMI"
+#
+
+PATH=/usr/bin:/usr/sbin
+
+. /lib/svc/share/smf_include.sh
+
+USAGE="Usage: $0 <method>"
+
+if [ $# -ne 1 ] ; then
+    echo $USAGE
+    exit 2
+fi
+
+METHOD=$1
+
+case $METHOD in
+    start)
+	# Continue with rest of script
+	;;
+    -*)
+	echo $USAGE
+	exit 2
+	;;
+    *)
+	echo "Invalid method $METHOD"
+	exit 2
+	;;
+esac
+
+getprop() {
+    PROPVAL=""
+    svcprop -q -p $1 application/font/fc-cache
+    if [ $? -eq 0 ] ; then
+	PROPVAL=$(svcprop -p $1 application/font/fc-cache)
+	if [ "$PROPVAL" == "\"\"" ] ; then
+	    PROPVAL=""
+	fi
+	return
+    fi
+    return
+}
+
+ARGS=""
+POSTCMD=""
+RETVAL=$SMF_EXIT_OK
+
+getprop options/force_rebuild
+if [ "$PROPVAL" = "true" ] ; then
+    ARGS="$ARGS -f"
+    POSTCMD="svccfg -s application/font/fc-cache setprop options/force_rebuild=false"
+fi
+
+/usr/bin/fc-cache $ARGS
+if [ $? -ne 0 ] ; then
+    RETVAL=$SMF_EXIT_MON_DEGRADE
+fi
+
+if [ "$POSTCMD" != "" ] ; then
+    eval $POSTCMD
+fi
+
+exit $RETVAL
+
