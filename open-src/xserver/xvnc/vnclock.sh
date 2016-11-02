8a80c743c60177dc720034dff75cf0cd3cdf796b
Niveditha Rau <Niveditha.Rau@Oracle.COM>
2016-11-02 08:43:19 -0700

    25022410 vnc changes for gnome3

--- open-src/xserver/xvnc/vnclock.sh
+++ open-src/xserver/xvnc/vnclock.sh
@@ -3,6 +3,7 @@
 # Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
 #
 
-XAUTHORITY=/var/xauth/$1
-export XAUTHORITY
-xscreensaver-command -display :$1 -lock
+DISPLAY=":$1"
+XAUTHORITY="/var/xauth/$1"
+export XAUTHORITY DISPLAY
+/usr/bin/xdg-screensaver lock
